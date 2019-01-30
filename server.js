66692730abd463b1ad0d46e517e9e4f3b0a7e848
Atanas <atanasbochukov@gmail.com>
2019-01-30 13:03:16 +0200

    Made changes according new project structure

--- server.js
+++ server.js
@@ -1,26 +1,17 @@
 var express = require('express');
-var app = express();
 var mongoose = require('mongoose');
-// var morgan = require('morgan');
-// var bodyParser = require('body-parser');
-// var methodOverride = require('method-override');
+const app = express();
 
-mongoose.connect("mongodb://localhost:27017/rental", { useNewUrlParser: true });
+const carRouter = require('./routes/car.js');
 
-app.use(express.static(__dirname + '/public'));
-// app.use(morgan('dev'));
-// app.use(bodyParser.urlencoded({'extended':'true'}));
-// app.use(bodyParser.json());
-// app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-// app.use(methodOverride());
+const db = mongoose.connect('mongodb://localhost:27017/rental', { useNewUrlParser: true });
 
-app.listen(8080);
-console.log('App is now listening port 8080');
+app.use('/api/cars', carRouter);
 
-// app.get('/', function(req, res) {
-//   res.send('Hello World');
-// });
-//
-// var server = app.listen(3000, function(){
-//   console.log('just test port 3000');
+// app.get('*', function(req, res) {
+//   res.sendfile('./public/index.html');
 // })
+
+app.listen(5656, () => {
+  console.log('http://localhost:5656')
+})
