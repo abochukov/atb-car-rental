793e3fa68e58ad73e53c0bc184fda297ffe57104
Atanas <atanasbochukov@gmail.com>
2019-01-31 10:34:47 +0200

    Added cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var mongoose = require('mongoose');
+var cors = require('cors');
 const app = express();
 
 const carRouter = require('./routes/car.js');
 
 const db = mongoose.connect('mongodb://localhost:27017/rental', { useNewUrlParser: true });
 
+app.use(cors());
 app.use('/api/cars', carRouter);
 
 // app.get('*', function(req, res) {
