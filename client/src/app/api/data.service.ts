c20a32856a0b45d86a4152821bbfb2b6b8e5b29a
Atanas <atanasbochukov@gmail.com>
2019-02-15 16:34:26 +0200

    Added type of the function fetchAll()

--- client/src/app/api/data.service.ts
+++ client/src/app/api/data.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
+import { Observable } from 'rxjs/Observable';
+
 @Injectable()
 export class DataService {
 
@@ -9,7 +11,7 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
 
-  getAllCars() {
+  getAllCars(): Observable<any> {
     return this.http.get(`${this.uri}/api/cars`);
   }
 
