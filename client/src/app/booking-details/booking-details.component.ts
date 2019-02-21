a33e69f106ed151208f7c9262435ddf53b78d8e1
Atanas <atanasbochukov@gmail.com>
2019-02-21 14:30:46 +0200

    Added reactive form for reservation

--- client/src/app/booking-details/booking-details.component.ts
+++ client/src/app/booking-details/booking-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Car } from '../car';
 
 import { DataService } from '../api/data.service';
@@ -13,13 +13,31 @@ import { DataService } from '../api/data.service';
 export class BookingDetailsComponent implements OnInit {
 
   public car: Car;
+  bookingForm: FormGroup;
+  public formSubmitAttempt: boolean;
 
-  constructor(private dataService: DataService, private route: ActivatedRoute) { }
+  constructor(private dataService: DataService, private route: ActivatedRoute, private formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.dataService.getCarsById(this.route.snapshot.params.id).subscribe(res => {
       this.car = res;
     });
+
+    this.booking();
   }
 
+  public booking() {
+    this.bookingForm = this.formBuilder.group({
+      firstname: [null, [Validators.required]],
+      lastname: [null, [Validators.required]],
+      email: [null, [Validators.required, Validators.email]]
+    });
+  }
+
+  onSubmit() {
+    this.formSubmitAttempt = true;
+    if (this.bookingForm.valid) {
+      console.log('the form is valid');
+    }
+  }
 }
