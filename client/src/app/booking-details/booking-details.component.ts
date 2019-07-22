c463f8a3933234d7c54ac15fbccbb31adb01e421
Atanas <atanasbochukov@gmail.com>
2019-07-22 14:24:56 +0300

    Some changes

--- client/src/app/booking-details/booking-details.component.ts
+++ client/src/app/booking-details/booking-details.component.ts
@@ -22,15 +22,13 @@ export class BookingDetailsComponent implements OnInit {
   bookingForm: FormGroup;
   public formSubmitAttempt: boolean;
   public showBookingForm: boolean;
-  public bsRangeValue: any;
-  public showDatePicker = false;
-  public fromDate: any;
-  public toDate: any;
-  // private fromDate: string;
   public storedDates: any;
-  // private isFree: boolean;
-  public disabledDates: any;
-
+  // public formattedUserFromDate: any;
+  // public formattedUserToDate: any;
+  public customerFromDate: any;
+  public customerToDate: any;
+  public storedFromDate: any;
+  public storedToDate: any;
 
   displayMonths = 2;
   navigation = 'select';
@@ -43,12 +41,7 @@ export class BookingDetailsComponent implements OnInit {
     private dataService: DataService,
     private route: ActivatedRoute,
     private formBuilder: FormBuilder,
-    // calendar: NgbCalendar
-    // private http: HttpClient
-  ) {
-    // this.fromDate = calendar.getToday();
-    // this.toDate = calendar.getToday();
-  }
+  ) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.params.id;
@@ -62,58 +55,52 @@ export class BookingDetailsComponent implements OnInit {
 
   public booking() {
     this.bookingForm = this.formBuilder.group({
-      firstname: [null, [Validators.required]],
-      lastname: [null, [Validators.required]],
-      email: [null, [Validators.required, Validators.email]],
+      firstname: ['test', [Validators.required]],
+      lastname: ['test', [Validators.required]],
+      email: ['test@test.com', [Validators.required, Validators.email]],
       carId: this.id,
       bookedDates: [null]
-      // dateRange: [this.fromDate],
-      // toDate: [this.toDate]
     });
   }
 
 
   // TRANSFORM DATES
   formatDate() {
-    const customerFromDate = this.bookingForm.value.bookedDates[0];
-    // console.log(customerFromDate);
-    const formattedFromDate = customerFromDate.getDate() + '-' + (customerFromDate.getMonth() + 1) + '-' + customerFromDate.getFullYear();
-    const customerToDate = this.bookingForm.value.bookedDates[1];
-    const formattedToDate = customerToDate.getDate() + '-' + (customerToDate.getMonth() + 1) + '-' + customerToDate.getFullYear();
-    // console.log(formattedFromDate);
-    // console.log(formattedToDate);
+    this.customerFromDate = this.bookingForm.value.bookedDates[0];
+    this.customerToDate = this.bookingForm.value.bookedDates[1];
   }
 
 
   getFreeCar() {
+    // const today = new Date().getDate() + '-' + (new Date().getMonth() + 1) + '-' + new Date().getFullYear();
     this.id = this.route.snapshot.params.id; // da se izvede w nova promenliva
     this.dataService.getBookingDate(this.route.snapshot.params.id).subscribe(bookings => {
       bookings.map(booking => {
         this.storedDates = booking.bookedDates;
-        console.log(this.storedDates);
-        // transfrom incoming date and disable in calendar
-        const storedFromDate = new Date(this.storedDates.split(',')[0]);
-        const formattedStoredFromDate = storedFromDate.getDate() + '-' + (storedFromDate.getMonth() + 1) + '-' + storedFromDate.getFullYear();
-        //
-        const storedToDate = new Date(this.storedDates.split(',')[1]);
-        const formattedStoredToDate = storedToDate.getDate() + '-' + (storedToDate.getMonth() + 1) + '-' + storedToDate.getFullYear();
-
-        console.log(formattedStoredFromDate);
-        console.log(formattedStoredToDate);
-
-        // this.disabledDates = [
-        //   // new Date(formattedStoredFromDate),
-        //   // new Date(formattedStoredToDate)
-        //   new Date('2019-03-01'),
-        //   new Date('2019-03-08')
-        // ];
+        this.storedFromDate = new Date(this.storedDates.split(',')[0]);
+        this.storedToDate = new Date(this.storedDates.split(',')[1]);
       });
     });
   }
 
+  public checkDate(a_start: any, a_end: any, b_start: any, b_end: any) {
+    a_start = this.customerFromDate;
+    a_end = this.customerToDate;
+    b_start = this.storedFromDate;
+    b_end = this.storedToDate;
+
+    if (a_start <= b_start && b_start <= a_end) { console.log('in range'); }  // b starts in a
+    if (a_start <= b_end   && b_end   <= a_end) { console.log('in range'); }  // b ends in a
+    if (b_start <  a_start && a_end   <  b_end) { console.log('in range'); }  // a in b
+    console.log('NOT in range');
+  }
+
+
+
   onSubmit() {
     this.formSubmitAttempt = true;
     this.formatDate();
+    this.checkDate(1, 2, 3, 4);
     // const frmD = this.bookingForm.value.fromDate;
     // console.log(Object.values(frmD));
     // console.log(this.bookingForm.value);
